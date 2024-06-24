5cb0e774396daddfe919fc11ef295b2559a10693
Shantnav Agarwal <shantnavagarwal@gmail.com>
2024-06-24 22:27:40 +0200

    prometheus rules and grafana dashboard updated

--- local_minikube_deploy.sh
+++ local_minikube_deploy.sh
@@ -6,6 +6,8 @@ helm repo add prom-repo https://prometheus-community.github.io/helm-charts
 helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
 helm repo update
 
-helm install -f kubernetes/charts/urlapp-kube-prom-stack-values.yaml prometheus prom-repo/kube-prometheus-stack
+helm upgrade -f kubernetes/charts/urlapp-kube-prom-stack-values.yaml prometheus prom-repo/kube-prometheus-stack
 helm upgrade --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard --create-namespace --namespace kubernetes-dashboard
 helm install testing-one kubernetes/charts/url_model
+
+kubectl apply -f kubernetes/charts/grafana_dashboard.yaml
