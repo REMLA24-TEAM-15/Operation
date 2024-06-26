98ae90acba7e0cf689e5f1627eeb284ce8f3ad98
Shantnav Agarwal <shantnavagarwal@gmail.com>
2024-06-26 22:19:03 +0200

    show helm name on app and configure chart accordingly. Got istio to divert traffic to two different pods

--- kubernetes/charts/istio_commands.sh
+++ kubernetes/charts/istio_commands.sh
@@ -1,8 +1,8 @@
 export PATH=/home/shantnav/Downloads/istio-1.22.0/bin:$PATH
 istioctl install
-kubectl apply -f ~/Downloads/istio-1.22.0/samples/addons/prometheus.yaml
-kubectl apply -f ~/Downloads/istio-1.22.0/samples/addons/jaeger.yaml
-kubectl apply -f ~/Downloads/istio-1.22.0/samples/addons/kiali.yaml
+# kubectl apply -f ~/Downloads/istio-1.22.0/samples/addons/prometheus.yaml
+# kubectl apply -f ~/Downloads/istio-1.22.0/samples/addons/jaeger.yaml
+# kubectl apply -f ~/Downloads/istio-1.22.0/samples/addons/kiali.yaml
 
 kubectl label ns default istio-injection=enabled
 
--- local_istio_minikube_deploy.sh
+++ local_istio_minikube_deploy.sh
@@ -1,8 +1,10 @@
-# minikube delete
+minikube delete
 
-# minikube start --memory=8192 --cpus=4 --driver=docker
+minikube start --memory=8192 --cpus=4 --driver=docker
 minikube addons enable ingress
 minikube addons enable metrics-server
+
+/bin/bash ./kubernetes/charts/istio_commands.sh
 
 helm repo add prom-repo https://prometheus-community.github.io/helm-charts
 helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
@@ -10,7 +12,7 @@ helm repo update
 
 sleep 10
 
-helm install -f kubernetes/charts/urlapp-kube-prom-stack-values.yaml prometheus prom-repo/kube-prometheus-stack
+helm install -f kubernetes/charts/urlapp-kube-prom-stack-values.yaml prometheus prom-repo/kube-prometheus-stack -n prometheus
 helm install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard --create-namespace --namespace kubernetes-dashboard
 helm install -f kubernetes/charts/testing-one-values.yaml testing-one kubernetes/charts/url_model
 
@@ -18,3 +20,4 @@ helm install -f kubernetes/charts/testing-two-values.yaml testing-two kubernetes
 
 
 kubectl apply -f kubernetes/charts/grafana_dashboard.yaml
+kubectl apply -f kubernetes/charts/istio_setup.yaml
